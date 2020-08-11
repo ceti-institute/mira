dbd8bf37530d30024b8615035d3ea75bb36c21e5
alannarisse <angrypirate@gmail.com>
2020-08-11 15:31:16 -0700

    added slides to about temp page

---
+++ js/slides.js
@@ -0,0 +1,15 @@
+
+$('.single-slide').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000,
+  infinite: true,
+  dots: true,
+  arrows: true,
+  // prevArrow: $('.prev'),
+  // nextArrow: $('.next')
+});
+
+
+
